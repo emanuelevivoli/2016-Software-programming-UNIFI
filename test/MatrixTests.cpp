1529431363313650c62b1db6f7187c35b68f4f79
Niccolò Bencini <niccolo.bencini@gmail.com>
2016-07-11 13:58:18 +0200

    Added Google Test library

---
+++ test/MatrixTests.cpp
@@ -0,0 +1,10 @@
+#include "gtest/gtest.h"
+
+#include "../Matrix.h"
+
+TEST(GameCharacter, ConstructorRowColumns) {
+    Matrix<int> m(3, 4);
+
+    ASSERT_EQ(3, m.getRows());
+    ASSERT_EQ(4, m.getColumns());
+}
